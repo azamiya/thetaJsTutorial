ba8e239c42f025ab184639a846ba0212f42ab65d
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-10-12 15:53:59 +0900

    rename to docs

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const http = require('http').Server(app);
 
 const server = require("./server");
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/docs'));
 server.start(app, http);
