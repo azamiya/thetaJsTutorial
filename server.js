d34ac0308ffc833a622baff6234a15ae9aa25523
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-10-12 15:50:46 +0900

    modify server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var PORT = 3330;
 
 function start(app, http) {
 	app.get('/', function(req, res) {
-	    res.sendFile(__dirname + "/client/index.html")
+	    res.sendFile(__dirname + "/docs/index.html")
 	});
 
 	app.get('/0.localStream', function(req, res) {
-	    res.sendFile(__dirname + "/client/padTutorial/index.html")
+	    res.sendFile(__dirname + "/docs/0.localStream/index.html")
 	});
 
 	http.listen(PORT, function(){
